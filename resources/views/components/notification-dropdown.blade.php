52b5bfc15c77d3a63998c3157b3bf0c35eded052
Faisal Abubaker <ahmadfaisal47@gmail.com>
2025-05-13 08:23:26 +0430

    notification issue fixed

--- resources/views/components/notification-dropdown.blade.php
+++ resources/views/components/notification-dropdown.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ 
+<div x-data="{
     open: false,
     count: 0,
     notifications: [],
@@ -27,24 +27,24 @@
             });
     }
 }" class="relative">
-    <button 
-        @click="open = !open" 
+    <button style="margin-right:20px;"
+        @click="open = !open"
         class="relative p-2 text-gray-600 hover:text-gray-900 focus:outline-none"
         :disabled="loading"
     >
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
         </svg>
-        <span 
-            x-show="count > 0" 
-            x-text="count" 
+        <span
+            x-show="count > 0"
+            x-text="count"
             class="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center"
         ></span>
     </button>
 
-    <div 
-        x-show="open" 
-        @click.away="open = false" 
+    <div style="width:250px;"
+        x-show="open"
+        @click.away="open = false"
         class="absolute right-0 mt-2 w-80 bg-white rounded-md shadow-lg overflow-hidden z-50"
         x-cloak
         style="display: none;"
@@ -53,7 +53,7 @@
             <template x-if="error">
                 <div class="py-4 text-center text-red-500" x-text="error"></div>
             </template>
-            
+
             <template x-if="!error && notifications.length === 0">
                 <div class="py-4 text-center text-gray-500">
                     {{ __('No new notifications') }}
@@ -82,8 +82,8 @@
                             </div>
                         </a>
                     </template>
-                    
-                    <a href="{{ route('notifications.index') }}" class="block bg-gray-50 text-center py-2 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+
+                    <a href="{{ route('notifications.index') }}" style="display:block; margin:20px;" class="block bg-gray-50 text-center py-2 text-sm font-medium text-indigo-600 hover:text-indigo-500">
                         {{ __('View All Notifications') }}
                     </a>
                 </div>
